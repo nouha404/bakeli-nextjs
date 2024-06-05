fb30514b133a6de1d026233e1a79a3a0224e1951
nouha404 <aboubacar.marega@ism.edu.sn>
2024-06-05 20:11:51 +0200

    backend chilling

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
+import { Main } from "@/styles/Login.style";
 import Image from "next/image";
 import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24 bg-primary_color">
+    <Main className="flex flex-col items-center justify-between min-h-screen p-24 bg-primary_color">
       <div className="w-4/12 h-[597.59px]">
         <header className="flex justify-around px-10 text-white  justify-centert-bold h-[50px]">
           <div className="flex logo">
@@ -26,19 +27,19 @@ export default function Home() {
         </header>
 
         <div className="content  h-[424.25px] border-2 border-white bg-white">
-            <h2 className=" pt-10 text-center ">Connectez-vous en tant que Admin</h2>
+            <h2 className="pt-10 text-center ">Connectez-vous en tant que Admin</h2>
             <div className="input flex flex-col justify-around h-[150px] items-center ">
                 <input type="text" placeholder="Email" className="text h-[45.33px] w-[270px] border-b border-gray-400 outline-none focus:border-none"/>
                 <input type="password" placeholder="Password" className="text h-[45.33px] w-[270px] border-b border-gray-400 outline-none focus:border-none"/>
             </div>
             <div className="checkbox mx-auto flex items-center justify-center justify-around w-[300px] pt-4 ">
                 <input type="checkbox" className="checkbox w-[20px] h-[20px] border-3 border-text-checkboxColor"/>
-                <p className="font-roboto text-base text-xl">Gardez-moi connecté</p>
+                <p className="text-base text-xl font-roboto">Gardez-moi connecté</p>
             </div>
 
             <button className="bg-buttonColor  font-roboto font-medium text-xl px-4 py-2 text-white rounded mt-10 w-[320px] mx-10">Se connecter</button>
 
-            <div className="flex flex-col p-4 bg-primary_color mt-3 text-center">
+            <div className="flex flex-col p-4 mt-3 text-center bg-primary_color">
                 <Link href="/password" className="text-linkColor">Mot de passe oublié?</Link>
                 <Link href="/sign-in" className="text-white">Vous n avez pas de compte? <span className="text-linkColor">S inscrire</span></Link>
             </div>
@@ -52,6 +53,6 @@ export default function Home() {
       
       
 
-    </main>
+    </Main>
   );
 }
