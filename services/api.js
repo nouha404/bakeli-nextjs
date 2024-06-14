21b424b90cd5c3cae4ef3939213bad9b8cc6f29b
nouha404 <aboubacar.marega@ism.edu.sn>
2024-06-14 20:31:45 +0200

    change url

--- services/api.js
+++ services/api.js
@@ -1,6 +1,6 @@
 import { toast } from 'react-toastify';
 
-const API_BASE_URL = "http://localhost:3007/api";
+const API_BASE_URL = "https://bakeli-backend.onrender.com/api";
 
 const fetchData = async (endpoint) => {
     try {
